67c094d0e4ada2e1dbed09791f5bf210bc2eb60b
oehimen1 <candaceehimen@gmail.com>
2022-04-18 00:06:56 -0400

    fixed port

--- portfolio_api/config.js
+++ portfolio_api/config.js
@@ -1,7 +1,8 @@
 require("dotenv").config()
 require("colors")
 
-const PORT = process.env.PORT ? Number(process.env.PORT) : 3001
+// const PORT = process.env.PORT ? Number(process.env.PORT) : 3001
+const PORT = process.env.PORT || 3001
 const SECRET_KEY = process.env.SECRET_KEY || "secret_dev"
 
 const IS_TESTING = process.env.NODE_ENV === "test"
